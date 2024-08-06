fe5e79662b618944fa370e454e5057e1f14d4ee9
Pavel Zhukov <paz80@yandex.ru>
2024-08-06 11:27:08 +0300

    5 commit

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -7,15 +7,70 @@ import java.util.Random;
 public class DinnerConstructor {
 
     Random random = new Random();
+    static HashMap<String, ArrayList<String>> dishList = new HashMap<>();
+    ArrayList<String> dishTypeCombo = new ArrayList<>();
 
+    void addDish() {
+        ArrayList<String> newTypeList1 = new ArrayList<>();
+        newTypeList1.add("Щи");
+        newTypeList1.add("Борщ");
+        newTypeList1.add("Уха");
+        dishList.put("Суп", newTypeList1);
+
+        ArrayList<String> newTypeList2 = new ArrayList<>();
+        newTypeList2.add("Отбивная");
+        newTypeList2.add("Азу");
+        newTypeList2.add("Стейк");
+        dishList.put("Мясо", newTypeList2);
+
+        ArrayList<String> newTypeList3 = new ArrayList<>();
+        newTypeList3.add("Рис");
+        newTypeList3.add("Булгур");
+        newTypeList3.add("Гречка");
+        dishList.put("Гарнир", newTypeList3);
+
+        ArrayList<String> newTypeList4 = new ArrayList<>();
+        newTypeList4.add("Квас");
+        newTypeList4.add("Морс");
+        newTypeList4.add("Кола");
+        dishList.put("Напиток", newTypeList4);
+
+        ArrayList<String> newTypeList5 = new ArrayList<>();
+        newTypeList5.add("Оливье");
+        newTypeList5.add("Витаминный");
+        newTypeList5.add("Винегрет");
+        dishList.put("Салат", newTypeList5);
+
+        for (String string : dishList.keySet()) {
+            System.out.println("Теперь в типах: " + string + " следующие блюда: " + dishList.get(string));
+        }
+    }
 
     Boolean checkType(HashMap<String, ArrayList<String>> dishList, String dishType) {
         return dishList.containsKey(dishType);
     }
 
-    void dishGenerator(HashMap<String, ArrayList<String>> dishList, ArrayList<String> dishTypeCombo, int numberOfCombos) {
+    void dishGenerator(int numberOfCombos) {
         for (int i = 0; i < numberOfCombos; i++) {
-            System.out.println("Комбо №" + (i + 1) + ": " + dishTypeCombo.get(i)); // заведомо неправильно
+            ArrayList<String> comboMenu = new ArrayList<>();
+
+            for (String s : dishTypeCombo) {
+                comboMenu.add(dishRandomizer(dishList.get(s)));
+            }
+            System.out.println("Комбо " + (i + 1));
+            System.out.println(comboMenu);
+        }
+    }
+
+    String dishRandomizer(ArrayList<String> dishArrayList) {
+        return dishArrayList.get(random.nextInt(dishArrayList.size()));
+    }
+
+    void dishTypeChoice(String nextItem) {
+        if (checkType(dishList, nextItem)) {
+            dishTypeCombo.add(nextItem);
+        } else {
+            System.out.println("Такого типа блюд нет. Выберите другой");
         }
     }
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,15 +1,11 @@
 package ru.practicum.dinner;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     static DinnerConstructor dc;
     static Scanner scanner;
-
-    static HashMap<String, ArrayList<String>> dishList = new HashMap<>();
 
     public static void main(String[] args) {
         dc = new DinnerConstructor();
@@ -44,35 +40,7 @@ public class Main {
 //        String dishType = scanner.nextLine();
 //        System.out.println("Введите название блюда:");
 //        String dishName = scanner.nextLine();
-        ArrayList<String> newTypeList1 = new ArrayList<>();
-        newTypeList1.add("Щи");
-        newTypeList1.add("Борщ");
-        newTypeList1.add("Уха");
-        dishList.put("Суп", newTypeList1);
 
-        ArrayList<String> newTypeList2 = new ArrayList<>();
-        newTypeList2.add("Отбивная");
-        newTypeList2.add("Азу");
-        newTypeList2.add("Стейк");
-        dishList.put("Мясо", newTypeList2);
-
-        ArrayList<String> newTypeList3 = new ArrayList<>();
-        newTypeList3.add("Рис");
-        newTypeList3.add("Булгур");
-        newTypeList3.add("Гречка");
-        dishList.put("Гарнир", newTypeList3);
-
-        ArrayList<String> newTypeList4 = new ArrayList<>();
-        newTypeList4.add("Квас");
-        newTypeList4.add("Морс");
-        newTypeList4.add("Кола");
-        dishList.put("Напиток", newTypeList4);
-
-        ArrayList<String> newTypeList5 = new ArrayList<>();
-        newTypeList5.add("Оливье");
-        newTypeList5.add("Витаминный");
-        newTypeList5.add("Винегрет");
-        dishList.put("Салат", newTypeList5);
 
 //        if (dishList.containsKey(dishType)) {
 //            dishList.get(dishType).add(dishName);
@@ -82,14 +50,10 @@ public class Main {
 //            dishList.put(dishType, newTypeList);
 //        }
 
-        for (String string : dishList.keySet()) {
-            System.out.println("Теперь в типах: " + string + " следующие блюда: " + dishList.get(string));
-        }
+        dc.addDish();
     }
 
     private static void generateDishCombo() {
-
-        ArrayList<String> dishTypeCombo = new ArrayList<>();
 
         System.out.println("Начинаем конструировать обед...");
 
@@ -100,20 +64,13 @@ public class Main {
         System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
         while (true) {
             String nextItem = scanner.nextLine();
-            //реализация ввода типов блюд
-            if (!nextItem.isEmpty()) {
-                if (dc.checkType(dishList, nextItem)) {
-                    dishTypeCombo.add(nextItem);
-                } else {
-                    System.out.println("Такого типа блюд нет. Выберите другой");
-                }
+
+            if (!nextItem.isEmpty()) { //реализация ввода типов блюд
+                dc.dishTypeChoice(nextItem);
             } else {
-                dc.dishGenerator(dishList, dishTypeCombo, numberOfCombos);
+                dc.dishGenerator(numberOfCombos);
                 break;
             }
-
-        }
-
-        // сгенерируйте комбинации блюд и выведите на экран
+        } // генерация комбинации блюд и выведите на экран
     }
 }
